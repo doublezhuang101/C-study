2b748250721cee40218ac09f6bcad55fa09ff524
壮 苗 <doublezhuang101@hotmail.com>
2019-03-25 20:59:05 +0800

    累了

---
+++ C-study/C-study/2018023417_3-3-22.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <Cmath>
+using namespace std;
+class CFraction
+{
+private:
+	int nume; 
+	int deno;  
+public:
+	CFraction(int nu = 0, int de = 1) :nume(nu), deno(de) {}
+	friend istream &operator>>(istream &in, CFraction &x);
+	friend ostream &operator<<(ostream &out, CFraction x);
+
+	CFraction operator+(const CFraction &c);  //两个分数相加，结果要化简
+};
+
+
+istream &operator>>(istream &in, CFraction &x)
+{
+	char ch;
+	while (1)
+	{
+		cin >> x.nume >> ch >> x.deno;
+		if (x.deno == 0)
+			cerr << "分母为0, 请重新输入\n";
+		else if (ch != '/')
+			cerr << "格式错误（形如m/n）! 请重新输入\n";
+		else
+			break;
+	}
+	return cin;
+}
+
+ostream &operator<<(ostream &out, CFraction x)
+{
+	cout << x.nume << '/' << x.deno;
+	return cout;
+}
+
+CFraction CFraction::operator+(const CFraction &c)
+{
+	CFraction t;
+	t.nume = nume * c.deno + c.nume*deno;
+	t.deno = deno * c.deno;
+	return t;
+}
+
+
+int main()
+{
+	CFraction x, y, s;
+	cout << "输入x: ";
+	cin >> x;
+	cout << "输入y: ";
+	cin >> y;
+	cout << "x+y=" << x+y << endl;
+	return 0;
+}
